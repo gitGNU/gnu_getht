4604efbcfff9f66a520cda478dd285bd5b1afd83
Nick White <git@njw.me.uk>
2010-06-28 21:50:30 +0100

    Simplify make include

--- config.mk
+++ config.mk
@@ -7,17 +7,17 @@ PREFIX = /usr/local
 MANPREFIX = ${PREFIX}/share/man
 
 # includes and libs
-INCS = -I. -I/usr/include
-LIBS = -L/usr/lib -lc
-
 CURLINC = $(shell pkg-config --cflags libcurl)
 CURLLIB = $(shell pkg-config --libs libcurl)
 XMLINC = $(shell pkg-config --cflags libxml-2.0)
 XMLLIB = $(shell pkg-config --libs libxml-2.0)
 
+INCS = -I. -I/usr/include ${CURLINC} ${XMLINC}
+LIBS = -L/usr/lib -lc ${CURLLIB} ${XMLLIB}
+
 # flags
-CFLAGS = -std=c99 -Wall -Werror ${INCS} ${CURLINC} ${XMLINC} -DVERSION=\"${VERSION}\"
-LDFLAGS = ${LIBS} ${CURLLIB} ${XMLLIB}
+CFLAGS = -std=c99 -Wall -Werror ${INCS} -DVERSION=\"${VERSION}\"
+LDFLAGS = ${LIBS}
 
 # compiler and linker
 CC = cc
